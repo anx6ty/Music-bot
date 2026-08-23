a8541c9f4a82313061aa522c161277ba89d0871b
anx6ty <anx6ty@gmail.com>
2026-08-23 16:17:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,14 +45,14 @@ CUSTOM_EMOJI_IDS = {
 
 # unicode fallbacks used until you fill in real IDs above
 _FALLBACK_EMOJI = {
-    "pause":      "\<:pause1:1541035966519705611>",
-    "resume":     "\<:resume1:1541036052591018028>",
-    "skip":       "\<:skip1:1541038015751659540>",
-    "shuffle":    "\<:shuffle1:1541036128558391357>",
+    "pause":      ":1541035966519705611:",
+    "resume":     ":1541036052591018028:",
+    "skip":       ":1541038015751659540:",
+    "shuffle":    ":1541036128558391357:",
     "loop_off":   "🔁",
     "loop_track": "🔂",
     "loop_queue": "🔁",
-    "stop":    "\<:stop1:1541029774477230150>",
+    "stop":       ":1541029774477230150:",
 }
 
 
